29c884c6ab385ef67d9aa656ebb7c94cabeb5c35
myuhe <yuhei.maeda@gmail.com>
2015-08-29 13:39:31 +0900

    Fix doc

--- quick-preview.el
+++ quick-preview.el
@@ -30,6 +30,10 @@
 ;; Add to .emacs:
 ;; (require 'quick-preview)
 ;;
+;;#Setting for key bindings
+;; (global-set-key (kbd "C-c q") 'quick-preview-at-point)
+;; (define-key dired-mode-map (kbd "Q") 'quick-preview-at-point)
+;;
 ;;; Changelog:
 ;; 2015-08-22 Initial release.
 
